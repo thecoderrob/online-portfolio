11e46c1a713c3097de23624d0cb5cd7c9d71af97
Robin Regodon <robinregodon07@gmail.com>
2022-11-19 00:40:27 +0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import React from "react";
 
-function App() {
-  return <div className="App">xd</div>;
-}
+const App = () => {
+  return <div className="App"></div>;
+};
 
 export default App;
